9c736f80e6683f30041b2cd82dc46362df07896a
ChoiMinSang <min393917@gmail.com>
2025-02-18 10:23:35 +0900

    v1.8 day07 debugger

--- day07.py
+++ day07.py
@@ -1,24 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.items = list()
-
-    def push(self, data):
-        self.items.append(data)
-
-    def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def peek(self):
-        return self.items[-1]
+def a(i):
+    j = 9
+    print(i, j)
 
 
-s1 = Stack()
-s1.push(-9)
-s1.push(11)
-s1.push(977)
-print(s1.pop())
-print(s1.peek())
-print(s1.peek())
+def b(k):
+    a(1)
+    print(k)
+
+
+def main():
+    print("start")
+    b(3)
+    print("end")
+
+
+if __name__ == "__main__":
+    main()
